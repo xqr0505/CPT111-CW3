e3d517a6f275eec448c49b921e40cbaf521c72a2
xqr0505 <sys2010xiaoqiran@126.com>
2024-11-24 16:04:52 +0800

    adjust the layout of quiz page

--- src/UI/QuizPage.java
+++ src/UI/QuizPage.java
@@ -83,6 +83,7 @@ public void start(Stage primaryStage) {
     RadioButton optionButton  =   new RadioButton();
     optionButton.setToggleGroup(optionsGroup);
     optionButton.setPrefWidth(500);
+    optionButton.setStyle("-fx-padding: 5 5 0 70;");
     optionButtons.add(optionButton);
   }
 
@@ -124,7 +125,7 @@ public void start(Stage primaryStage) {
   root.getChildren().add(buttonBox);
 
   // Create scene
-  Scene scene = new Scene(root, 600, 400);
+  Scene scene = new Scene(root, 500, 400);
 
   // Set stage
   primaryStage.setTitle("Quiz - " + subject);
